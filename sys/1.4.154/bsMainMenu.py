1ac782aaad2ec63e8cb3214c8196d9dee810a971
Delitel-WEB <delitel1@mail.ru>
2020-08-20 01:20:04 +0300

    v1.4

--- sys/1.4.154/bsMainMenu.py
+++ sys/1.4.154/bsMainMenu.py
@@ -20,10 +20,17 @@ class MainMenuActivity(bs.Activity):
         def menu_music():
             if env['platform'] == 'android':
                 file = "menuMusic.ogg"
+                whenJohnnyComesMarchingHomeMusic = "whenJohnnyComesMarchingHomeMusic.ogg"
                 if file in os.listdir(env['userScriptsDirectory']):
                     import shutil # copy our file to bombsquad audios data
                     try: shutil.copy(os.path.join(env['userScriptsDirectory'], file), '/data/data/net.froemling.bombsquad/files/bombsquad_files/data/audio')
                     except Exception as E: print(E)
+
+                if whenJohnnyComesMarchingHomeMusic in os.listdir(env['userScriptsDirectory']):
+                    import shutil # copy our file to bombsquad audios data
+                    try: shutil.copy(os.path.join(env['userScriptsDirectory'], file), '/data/data/net.froemling.bombsquad/files/bombsquad_files/data/audio')
+                    except Exception as E: print(E) 
+
         menu_music()
 
     def fireworks(self):
