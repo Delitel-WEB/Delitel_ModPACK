ebfbfe8d0884b53e08b8ceb3353627130fe84648
Delitel-WEB <delitel1@mail.ru>
2020-08-20 04:16:09 +0300

    v1.5

--- upload_music.py
+++ upload_music.py
@@ -2,7 +2,7 @@ import shutil
 
 victory = "victoryMusic.ogg"
 
-if env['platform'] == 'android':
-	if file in os.listdir(env['userScriptsDirectory']):
-		try: shutil.copy(os.path.join(env['userScriptsDirectory'], file), '/data/data/net.froemling.bombsquad/files/bombsquad_files/data/audio')
-		except Exception as E: print(E)
+
+if file in os.listdir(env['userScriptsDirectory']):
+	try: shutil.copy(os.path.join(env['userScriptsDirectory'], file), '/data/data/net.froemling.bombsquad/files/bombsquad_files/data/audio')
+	except Exception as E: print(E)
