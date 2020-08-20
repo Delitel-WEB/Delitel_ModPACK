60680c68d6d7ef64c01a05bb6eb01be2329fe91b
Delitel-WEB <delitel1@mail.ru>
2020-08-20 04:15:22 +0300

    v1.5

--- upload_music.py
+++ upload_music.py
@@ -4,5 +4,5 @@ victory = "victoryMusic.ogg"
 
 if env['platform'] == 'android':
 	if file in os.listdir(env['userScriptsDirectory']):
-        try: shutil.copy(os.path.join(env['userScriptsDirectory'], file), '/data/data/net.froemling.bombsquad/files/bombsquad_files/data/audio')
-        except Exception as E: print(E)
+		try: shutil.copy(os.path.join(env['userScriptsDirectory'], file), '/data/data/net.froemling.bombsquad/files/bombsquad_files/data/audio')
+		except Exception as E: print(E)
